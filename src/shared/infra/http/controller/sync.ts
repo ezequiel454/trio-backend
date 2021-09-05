cc8b0ad4ec98145b43d97c1b6362549723ffa2cb
zeke <ezequiel454@gmail.com>
2021-09-04 21:44:30 -0300

    fix path

--- src/shared/infra/http/controller/sync.ts
+++ src/shared/infra/http/controller/sync.ts
@@ -1,6 +1,6 @@
 import { createContact } from '../../../../modulos/contact/controller/contact'
-import { getListAudience } from 'shared/infra/provider/mailchimp/controller/get-list-audience'
-import { getListId } from 'shared/infra/provider/mailchimp/controller/get-list-id'
+import { getListAudience } from '../../provider/mailchimp/controller/get-list-audience'
+import { getListId } from '../../provider/mailchimp/controller/get-list-id'
 import { Request, Response } from 'express'
 
 export const syncContact = async (req: Request, res: Response) => {
