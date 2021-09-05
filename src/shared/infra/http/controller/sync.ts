09c820dc024519e636354d39735b44f8b36f347f
zeke <ezequiel454@gmail.com>
2021-09-04 21:52:42 -0300

    Heroku

--- src/shared/infra/http/controller/sync.ts
+++ src/shared/infra/http/controller/sync.ts
@@ -4,10 +4,13 @@ import { getListId } from '../../provider/mailchimp/controller/get-list-id'
 import { Request, Response } from 'express'
 
 export const syncContact = async (req: Request, res: Response) => {
+	console.log('get mail')
 	const id = await getListId()
+	console.log(id)
 	const personList = await getListAudience(id)
-
+	console.log('get mail 2', personList)
 	const savedFirebase = await createContact(personList)
+	console.log('fb', savedFirebase)
 
 	return res.status(200).json({
 		data: {
