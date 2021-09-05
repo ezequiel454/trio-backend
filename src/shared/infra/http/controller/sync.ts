05e35f59bc6fac03cc85f52a434589e0cb2ccc6e
zeke <ezequiel454@gmail.com>
2021-09-04 22:18:53 -0300

    fix misspelling folder and response firebase

--- src/shared/infra/http/controller/sync.ts
+++ src/shared/infra/http/controller/sync.ts
@@ -1,4 +1,4 @@
-import { createContact } from '../../../../modulos/contact/controller/contact'
+import { createContact } from '../../../../modules/contact/controller/contact'
 import { getListAudience } from '../../../provider/mailchimp/controller/get-list-audience'
 import { getListId } from '../../../provider/mailchimp/controller/get-list-id'
 import { Request, Response } from 'express'
@@ -6,11 +6,11 @@ import { Request, Response } from 'express'
 export const syncContact = async (req: Request, res: Response) => {
 	const id = await getListId()
 	const personList = await getListAudience(id)
-	const savedFirebase = await createContact(personList)
+	const { contacts } = await createContact(personList)
 
 	return res.status(200).json({
 		data: {
-			contacts: savedFirebase,
+			contacts,
 		},
 	})
 }
